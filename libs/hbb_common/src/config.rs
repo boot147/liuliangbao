810fcb988aadd25d843e2d80c1a169e1655cf3b5
boot147 <sHJriBRa3E4jvrXTH@outlook.com>
2024-10-07 07:50:58 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["208.76.223.217"];
+pub const PUBLIC_RS_PUB_KEY: &str = "l0S4efMpD3HajsTS6H1ZUmK7DNr3rP3JY1BP3SSOZ4g=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
